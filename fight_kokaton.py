23f9fed43083b3edfdb02c1789dfa139eb3fa740
c0b2313978 <c0b2313978@edu.teu.ac.jp>
2024-12-03 17:12:33 +0900

    演習4:向きに応じたビーム

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -1,3 +1,4 @@
+import math
 import os
 import random
 import sys
@@ -56,6 +57,7 @@ class Bird:
         self.img = __class__.imgs[(+5, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
+        self.dire = (5, 0)
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -82,6 +84,7 @@ class Bird:
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):
             self.img = __class__.imgs[tuple(sum_mv)]
+            self.dire = sum_mv
         screen.blit(self.img, self.rct)
 
 
@@ -96,9 +99,15 @@ class Beam:
         """
         self.img = pg.image.load(f"fig/beam.png")
         self.rct = self.img.get_rect()
-        self.rct.centery = bird.rct.centery
-        self.rct.left = bird.rct.right
-        self.vx, self.vy = +5, 0
+        # self.rct.centery = bird.rct.centery
+        # self.rct.left = bird.rct.right
+
+        self.vx, self.vy = bird.dire
+        self.angle = math.degrees(math.atan2(-self.vy, self.vx))
+        self.img = pg.transform.rotozoom(self.img, self.angle, 0.9)
+
+        self.rct.centerx = bird.rct.centerx + bird.img.get_width()*self.vx/5
+        self.rct.centery = bird.rct.centery + bird.img.get_height()*self.vy/5
 
     def update(self, screen: pg.Surface):
         """
